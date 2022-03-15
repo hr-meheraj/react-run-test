a516c95b2ef4b25b4fc2c49511672028aa7d892d
Habibur Rahman Meheraj <97170117+hr-meheraj@users.noreply.github.com>
2022-03-15 15:22:06 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	const name = "Habib";
+  return (
+    <main>
+			<h2> {name} </h2>
+      React⚛️ + Vite⚡ + Replit🌀
+    </main>
+  );
+}
+
+export default App;
